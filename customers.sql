f217d7c24e9d78019c895364c005f1a691cae390
WSandford93 <121934829+WSandford93@users.noreply.github.com>
2023-02-03 16:28:33 +0000

    Add customers query

--- customers.sql
+++ customers.sql
@@ -3,7 +3,7 @@ select
   c.name, 
   c.email, 
   min(o.created_at) as first_order_at, 
-  count(o.id) as number_of_orders
+  count(distinct o.id) as number_of_orders
 from `analytics-engineers-club.coffee_shop.customers` c
 left join `analytics-engineers-club.coffee_shop.orders` o on c.id = o.customer_id
 group by customer_id, name, email
